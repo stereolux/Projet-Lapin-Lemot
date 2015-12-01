bc645465bc18a17a3367ce14129908be506bc847
xseignard <xavier.seignard@gmail.com>
2015-12-01 20:53:26 +0100

    Updated conf for heroku

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,7 +1,7 @@
 'use strict';
 var Conf = {};
 Conf.APP_URL = process.env.APP_URL || 'http://localhost';
-Conf.MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost/lapinlemot';
+Conf.MONGO_URL = process.env.MONGOLAB_URI || 'mongodb://localhost/lapinlemot';
 Conf.REDIS_URL = process.env.REDIS_URL || 'redis://:@localhost:6379';
 Conf.PORT = process.env.PORT || 3000;
 Conf.API_VERSION = 1;
