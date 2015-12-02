0ec087f413c9b7f3c00c2052a88b51637f45f505
xseignard <xavier.seignard@gmail.com>
2015-12-02 03:53:25 +0100

    Updated conf

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -3,7 +3,7 @@ var Conf = {};
 Conf.APP_URL = process.env.APP_URL || 'http://localhost';
 Conf.MONGO_URL = process.env.MONGOLAB_URI || 'mongodb://localhost/lapinlemot';
 Conf.REDIS_URL = process.env.REDIS_URL || 'redis://:@localhost:6379';
-Conf.PORT = (process.env.PORT === '') ? '' : 3000;
+Conf.PORT = process.env.PORT || 3000;
 Conf.API_VERSION = 1;
 Conf.API_PREFIX = '/api/' + Conf.API_VERSION;
 
--- src/misc/startVisit.js
+++ src/misc/startVisit.js
@@ -1,7 +1,11 @@
 var request = require('request'),
 	conf = require('../conf/conf.js');
 
-var postUrl = conf.APP_URL + ':' + conf.PORT + conf.API_PREFIX + '/visit'
+var postUrl = conf.APP_URL;
+if (conf.APP_URL === 'http://localhost') postUrl += ':' + conf.PORT;
+postUrl += conf.API_PREFIX + '/visit';
+
+console.log(postUrl);
 
 // TODO: link with arduino and NFC
 request.post(
